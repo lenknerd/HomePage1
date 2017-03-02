358263d5cb6f3e616ad0c1d789c4b287d9873c74
David Lenkner <lenknerd@gmail.com>
2017-03-01 22:56:01 -0500

    No longer need update-www since doing in gulp

--- update-www.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Script to do minification and processing, publish - runs gulpfile to do that. Sudo this.
-# David Lenkner, 2017
-
-# Go to where this script is
-cd "$( dirname "${BASH_SOURCE[0]}" )"
-# Go in one to the gulpfile directory, run it
-cd Serve
-gulp
